238f72d4dba4f4a1bf589100a8de7e1c42b98855
Liang Wang <102842456+liw873@users.noreply.github.com>
2024-06-30 09:39:11 -0700

    Create shebang.sh

---
+++ shebang.sh
@@ -0,0 +1,17 @@
+#! bin/bash
+
+# Print the prompt message on screen
+echo -n "Enter your name :"	  	
+
+# Wait for user to enter a name, and save the entered name into the variable \'name\'
+read name				
+
+# Print the welcome message followed by the name	
+echo "Welcome $name"
+
+## command lines
+## which bash
+## chmod +x greet.sh
+## chmod u+x greet.sh
+## ls -l greet.sh
+## ./greet.sh
